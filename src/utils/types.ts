3795fc2b3011219a86d2f86b5d032c7f6817d002
Utsu1279 <178627430+Utsu1279@users.noreply.github.com>
2024-10-17 14:02:06 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/utils/types.ts
@@ -0,0 +1,18 @@
+export abstract class Provider {
+  abstract getSource(
+    id: string,
+    isMovie: boolean,
+    season?: string,
+    episode?: string,
+    source?: string
+  ): Promise<Source | any>;
+}
+export interface Source {
+  url: string;
+  qualities?: { url: string; quality: string }[];
+  referer: string;
+  captions?: { url: string; language: string }[];
+}
+export type DecryptMethods = {
+  [key: string]: (inputString: string) => string;
+};
